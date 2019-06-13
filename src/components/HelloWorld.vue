ef91dcd9a3ec6204c8e28477ecf13cc732f120a3
byronzhao0519 <byron_zhao@163.com>
2019-06-13 11:46:45 +0800

    项目内容

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+       <div>
+    <treeselect :options="options" :value="value" :multiple="multiple">
+      <div slot="value-label" slot-scope="{ node }">{{ node.raw.customLabel }}</div>
+    </treeselect>
+    <p>
+      <label><input type="checkbox" v-model="multiple">Multi-select</label>
+    </p>
+  </div>
+  </div>
+</template>
+
+<script>
+// import the component
+  import Treeselect from '@riophae/vue-treeselect'
+  // import the styles
+  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+export default {
+  name: 'HelloWorld',
+   components: { Treeselect },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      multiple: true,
+      value: null,
+      options: [ 1, 2, 3 ].map(i => ({
+        id: i,
+        label: `Label ${i}`,
+        customLabel: `Custom Label ${i}`,
+      })),
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
